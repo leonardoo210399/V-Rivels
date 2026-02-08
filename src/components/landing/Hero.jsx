ddb9619f65daf7557d89810285fa98404adc6f39
leonardoo210399 <aditya210399@gmail.com>
2026-02-09 00:29:25 +0530

    feat: Add landing page hero and features sections, along with a maintenance page.

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -98,7 +98,7 @@ const Hero = () => {
   const getVideoSrc = (index) => `/videos/hero-${index}.mp4`;
 
   return (
-    <div className="relative h-dvh w-full overflow-x-hidden">
+    <div className="relative h-dvh w-full ">
       {loading && (
         <div className="flex-center absolute z-[100] h-dvh w-full overflow-hidden bg-rose-50">
           {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
